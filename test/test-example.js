cd5b97e04d9b835a80d2bdbb50b0ec4b615985a7
Aaron Surty <aaron.surty@gmail.com>
2011-10-07 12:14:36 -0400

    converting old tests to amd spec

--- test/test-example.js
+++ test/test-example.js
@@ -1,7 +1,7 @@
 define(['exports'], function(exports) {
 
     exports.testSomething = function(test) {
-        text.expect(1);
+        test.expect(1);
         test.ok(true, "this assertion should pass");
         test.done();
     };

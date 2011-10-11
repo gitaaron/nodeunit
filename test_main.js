92b716505dc08c57429b9f5d7c06a822b034edf8
Aaron Surty <aaron.surty@gmail.com>
2011-10-11 11:16:50 -0400

    making it possible to run from other directories

--- test_main.js
+++ test_main.js
@@ -1,22 +1,25 @@
 require(['fs', 'path'], function(fs, path) {
 
 
+
     var files = [];
+
     var reporter_path = 'lib/reporters/';
     var reporter_module = reporter_path + 'default';
     var args = process.ARGV.slice(2);
-    var content = fs.readFileSync(path.resolve()+'/bin/nodeunit.json');
-    var options = JSON.parse(content);
+
+    var options_mod = require('./lib/options');
+    var options = options_mod.load();   
+
 
     args.forEach(function(arg) {
         if(arg.slice(0,11)=='--reporter=') {
             reporter_module = reporter_path + arg.slice(11);
         } else if(arg.slice(0,9)=='--output=') {
             options.output = arg.slice(9);
-        } else if(arg!='test_main.js') {
+        } else if(arg.indexOf('test_main.js')==-1) {
             files.push(arg);
         }
-
     });
 
     var testrunner = require(reporter_module);
